8c4ddc73deb794e86e2717d4efe9eb835888693f
Florian Schweizer <post.an.flo@gmail.com>
2021-12-01 12:03:56 +0100

    Initial Commit

---
+++ WidgetKitCourse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WidgetKitCourse
+//
+//  Created by Florian Schweizer on 01.12.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WidgetKitCourse/WidgetKitCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetKitCourseApp.swift
+//  WidgetKitCourse
+//
+//  Created by Florian Schweizer on 01.12.21.
+//
+
+import SwiftUI
+
+@main
+struct WidgetKitCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
